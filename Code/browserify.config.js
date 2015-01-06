c83840f2ba26fbd93d63b7c5cabe4b37a73daf3c
Owain Llewellyn <Owain.Llewellyn@akqa.com>
2015-01-06 16:48:45 -0500

    initial commit

---
+++ Code/browserify.config.js
@@ -0,0 +1,8 @@
+//third party libraries alias configurations for browserify
+
+var vendorPath = "./Build/assets/js/vendor/";
+
+module.exports = [
+    vendorPath + 'angular/angular.min.js:angular',
+    vendorPath + 'angular-route/angular-route.min.js:angular-route'
+];
